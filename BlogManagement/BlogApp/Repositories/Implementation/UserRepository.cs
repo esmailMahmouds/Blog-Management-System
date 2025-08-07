7458049320684b2ae3edd2bc38222e179c913a85
Mohamed Khaled <mohamed.khaled1872002@gmail.com>
2025-08-07 19:05:58 +0300

    add project architecture layers as a template to use

---
+++ BlogManagement/BlogApp/Repositories/Implementation/UserRepository.cs
@@ -0,0 +1,26 @@
+﻿using BlogApp.Context;
+using BlogApp.Models;
+using BlogApp.Repositories.Interfaces;
+
+namespace BlogApp.Repositories.Implementation
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task AddUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User?> GetUserById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BlogManagement/BlogApp/UnitOfWork/Implementation/UnitOfWork.cs
@@ -0,0 +1,42 @@
+﻿using BlogApp.Context;
+using BlogApp.Repositories.Implementation;
+using BlogApp.Repositories.Interfaces;
+using BlogApp.UnitOfWork.Interfaces;
+
+namespace BlogApp.UnitOfWork.Implementation
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+
+        private readonly ApplicationDbContext _context;
+
+
+        private IUserRepository _userRepository;
+
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IUserRepository UserRepository
+        {
+            get
+            {
+                return _userRepository ??= new UserRepository(_context);
+            }
+        }
+
+
+        public async Task<int> Save()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+
+}
