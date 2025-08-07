124f0d7ee08982bdbd7e04fb9adbd200b18af7ee
Mohamed Khaled <mohamed.khaled1872002@gmail.com>
2025-08-07 12:58:47 +0300

    add swagger and a test endpoint

--- BlogManagement/BlogApp/Program.cs
+++ BlogManagement/BlogApp/Program.cs
@@ -7,18 +7,30 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(
-	options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
+
+// Add API Explorer services for Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-	app.UseExceptionHandler("/Home/Error");
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+else
+{
+    // Enable Swagger in development only
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/ping", () => Results.Ok("pong"));
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
@@ -28,7 +40,8 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-	name: "default",
-	pattern: "{controller=Home}/{action=Index}/{id?}");
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
 
 app.Run();
