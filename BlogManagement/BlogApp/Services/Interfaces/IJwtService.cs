e61292a64e09bcc830e7d5dd155da3e2a9e20586
Mohamed Khaled <mohamed.khaled1872002@gmail.com>
2025-08-09 23:39:59 +0300

    Add JWT and authentication services and extensions

---
+++ BlogManagement/BlogApp/Services/Interfaces/IJwtService.cs
@@ -0,0 +1,10 @@
+﻿using BlogApp.Models.DomainClasses;
+
+namespace BlogApp.Services.Interfaces
+{
+    public interface IJwtService
+    {
+        string GenerateToken(User user);
+        int GetUserIdFromToken(string token);
+    }
+}
