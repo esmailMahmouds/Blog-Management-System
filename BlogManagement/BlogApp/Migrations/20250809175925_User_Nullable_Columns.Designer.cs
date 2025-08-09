f2399adc1bfd0ef96808e93e4249a3c93d238a03
Mohamed Khaled <mohamed.khaled1872002@gmail.com>
2025-08-09 23:37:41 +0300

    Add migration for some user fields nullability

--- BlogManagement/BlogApp/Migrations/20250809175925_User_Nullable_Columns.Designer.cs
+++ BlogManagement/BlogApp/Migrations/20250809175925_User_Nullable_Columns.Designer.cs
@@ -4,6 +4,7 @@ using BlogApp.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BlogApp.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250809175925_User_Nullable_Columns")]
+    partial class User_Nullable_Columns
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
