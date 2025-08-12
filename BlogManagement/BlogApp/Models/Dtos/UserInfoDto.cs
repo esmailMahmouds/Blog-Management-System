f6264496d6da64f58b709d2352406a81a2221ff9
esmailMahmouds <120949930+esmailMahmouds@users.noreply.github.com>
2025-08-12 19:25:11 +0300

    Add profileimage to user

---
+++ BlogManagement/BlogApp/Models/Dtos/UserInfoDto.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogApp.Models.Dtos
+{
+    public class UserInfoDto
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [StringLength(254, MinimumLength = 3)]
+        public string Email { get; set; } = string.Empty;
+        
+        [Required]
+        public DateOnly DateOfBirth { get; set; }
+
+        [Required]
+        public int CountryId { get; set; }
+
+        public IFormFile? ProfileImage { get; set; }
+        public byte[]? ProfileImageBytes { get; set; }
+        public string? ProfileImageBase64
+        {
+            get
+            {
+                if(ProfileImageBytes != null && ProfileImageBytes.Length > 0)
+                {
+                    return Convert.ToBase64String(ProfileImageBytes);
+                }
+                return null;
+            }
+        }
+    }
+}
