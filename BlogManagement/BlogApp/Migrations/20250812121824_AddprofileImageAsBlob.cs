efd1df9bf74e939439bb69d30f61de40db378afc
esmailMahmouds <120949930+esmailMahmouds@users.noreply.github.com>
2025-08-12 19:24:37 +0300

    add profile page Migration

---
+++ BlogManagement/BlogApp/Migrations/20250812121824_AddprofileImageAsBlob.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlogApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddprofileImageAsBlob : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageURL",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "AverageRate",
+                table: "Users",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "ProfileImage",
+                table: "Users",
+                type: "varbinary(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfileImage",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageURL",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "AverageRate",
+                table: "Users",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+        }
+    }
+}
